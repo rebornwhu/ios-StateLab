16b41e3a1990ee3d0cdf11be582ae1c00c655bcb
rebornwhu <rebornwhu@gmail.com>
2015-12-13 20:02:05 -0800

    Implement animation

--- State Lab/ViewController.swift
+++ State Lab/ViewController.swift
@@ -9,10 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let bounds = view.bounds
+        let labelFrame:CGRect = CGRectMake(bounds.origin.x, CGRectGetMidY(bounds) - 50, bounds.size.width, 100)
+        label = UILabel(frame: labelFrame)
+        label.font = UIFont(name: "Helvetica", size: 70)
+        label.text = "Bazinga"
+        label.textAlignment = NSTextAlignment.Center
+        label.backgroundColor = UIColor.clearColor()
+        view.addSubview(label)
+        
+        self.rotateLabelDown()
+    }
+    
+    func rotateLabelDown() {
+        UIView.animateWithDuration(
+            0.5,
+            animations: {
+                self.label.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
+            },
+            completion: {
+                (bool) -> Void in self.rotateLabelUp()
+        })
+    }
+    
+    func rotateLabelUp() {
+        UIView.animateWithDuration(0.5,
+            animations: {
+                self.label.transform = CGAffineTransformMakeRotation(0)
+            },
+            completion: {
+                (bool) -> Void in self.rotateLabelDown()
+        })
     }
 
     override func didReceiveMemoryWarning() {
